52cf5a0207d60e73e830d75fc52b71fdfac48aca
BillC424 <wcurran44@gmail.com>
2022-10-01 18:18:52 -0400

    Added an empty result array to store sorted solution. Added a few loops to compare the left/right arrays and to add elements to the result array and delete from left/right array after comparisions are made. The merge_sort method now can sort arrays of numbers

--- recursion.rb
+++ recursion.rb
@@ -19,14 +19,45 @@ def fibs_rec(n, fibonacci=[0,1])
 end
 
 def merge_sort(array)
-    ## Base case
-    return array if array.length <= 1
-    ## Recursive case
-    p left = array.each_slice((array.size / 2.0).round).to_a[0]
-    p right = array.each_slice((array.size / 2.0).round).to_a[1]
+  ## Base case
+  return array if array.length <= 1
   
-    left = merge_sort(left)
-    right = merge_sort(right)
+  # Recursive case
   
-    # return merge(left, right)
+  # Splits array as evenly as possible
+  p left = array.each_slice((array.size / 2.0).round).to_a[0]
+  p right = array.each_slice((array.size / 2.0).round).to_a[1]
+  
+  # Recurusively call method on both sides of array
+  left = merge_sort(left)
+  right = merge_sort(right)
+  
+  # Empty array to store sorted solution in 
+  result = []
+  
+  # Compares the first element in the left and right arrays and adds lowest element to result array.
+  # If an element is added to the results array, it is also deleted from the left/right array so that the loop is not infinite. 
+  while left != [] && right != [] do
+    if left[0] < right[0]
+      result.push(left[0])
+      left.delete_at(0)
+    else
+      result.push(right[0])
+      right.delete_at(0)
+    end
   end
+  
+  # These two loops add remaining elements to result array when either the left or right array are empty. 
+  while left != [] do
+    result.push(left[0])
+    left.delete_at(0)
+  end
+  
+  while right != [] do
+    result.push(right[0])
+    right.delete_at(0)
+  end
+    
+  # Returns sorted solution 
+  result
+end
